3437afdf5521d1ba1d2a76d27c44086f09479ee4
lazinator1994 <84203550+lazinator1994@users.noreply.github.com>
2022-04-12 22:24:27 -0700

    basic code updates

--- rockpaper.js
+++ rockpaper.js
@@ -1,18 +1,59 @@
-let choices = ['rock','paper','scissors']
+let scissBtn = document.querySelector('#scissors')
+let rockBtn = document.querySelector('#rock')
+let paperBtn = document.querySelector('#paper')
+let textBox = document.querySelector('#placehere')
+let player = document.querySelector('#playerscore')
+let computer = document.querySelector('#computerscore')
 
+//set initiate values of scores to 0
+let computerS = 0;
+let playerS = 0;
 
-//create a function that generates a random computer choice
+//set scores to the dom.
+
+player.textContent = playerS
+computer.textContent = computerS
+//choices of the game
+let choices = ['rock', 'paper', 'scissors']
+
+//generte the computer choices
 function computerChoice(){
-    let randomC = choices[Math.floor(Math.random() * choices.length)]
-    console.log(randomC)
-    return randomC
+  let randomC = choices[Math.floor(Math.random() * choices.length)]
+  return randomC
 }
-computerChoice()
 
-//create a function that takes a players input and goes against the computer
-
-function playRound(){
-    let computer = computerChoice()
-    let humanChoice = 'rock'
+function playRound(playerC){
+  let computerC = computerChoice()
+  if(playerC === computerC){
+    textBox.textContent = 'The game is a tie'
+  } else if((playerC === 'rock' && computerC === 'scissors') || (playerC === 'paper' && computerC === 'rock') ||(playerC === 'scissors' && computerC === 'paper')){
+    playerS++
+    player.textContent = playerS
+    textBox.textContent = 'You beat the computer'
+    if(playerS === 5){
+      textBox.style.color = 'green'
+      textBox.textContent = 'You have bested the computer and won'
+    }
+  } else {
+    computerS++
+    textBox.textContent ='the computer beat you!'
+    computer.textContent = computerS
     
+    if(computerS === 5){
+      textBox.style.color = 'red';
+      textBox.textContent = 'You lost to a computer try again'
+    }
+  }
 }
+
+
+//event listeners for buttons
+rockBtn.addEventListener('click', () => {
+  playRound('rock')
+})
+scissBtn.addEventListener('click', () => {
+  playRound('scissors')
+})
+paperBtn.addEventListener('click', () => {
+  playRound('paper')
+})
